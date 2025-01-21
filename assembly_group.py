126e306822f08b36f65a4d85f8ce0fe4cbda2cb3
schwa <itkw456@gmail.com>
2025-01-21 23:57:36 +0900

    revised viewport meta tag

--- assembly_group.py
+++ assembly_group.py
@@ -125,12 +125,21 @@ if __name__ == '__main__':
     <!DOCTYPE html>
     <html>
         <head>
+            <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Mobile Optimized Scatter Plot</title>
+            {%metas%}
+            {%css%}
         </head>
         <body>
-            {%app_entry%}
-            <footer>{%config%}{%scripts%}</footer>
+            <div id="dash-app">
+                {%app_entry%}
+            </div>
+            <footer>
+                {%config%}
+                {%scripts%}
+                {%renderer%}
+            </footer>
         </body>
     </html>
     '''
